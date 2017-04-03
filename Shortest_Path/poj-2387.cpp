8239f4e65b37f40b4f113672b184f3f74d56c5dd
sync <zengzh1997@gmail.com>
2017-04-03 15:51:52 +0800

    Update poj-2387.cpp
    这个 是一道模板题，至于模板会不会忘我就不知道了

--- Shortest_Path/poj-2387.cpp
+++ Shortest_Path/poj-2387.cpp
@@ -1,1 +1,47 @@
 
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+const int max_num = 1010;
+const int INF = 1e9;
+int T ,N;
+int G[max_num + 1][max_num + 1] ;
+int d[max_num + 1];
+bool used[max_num + 1];
+void Dij()
+{
+    fill(d , d + N + 1 , INF);
+    fill(used , used + N + 1 , false);
+    d[1] = 0;
+    while(1){
+        int v = -1;
+        for(int u = 1 ; u <= N ; u++){
+            if(!used[u] && (v == -1 || d[u] < d[v])) v = u;
+        }
+
+        if(v == -1) break;
+        used[v] = true;
+
+        for(int u = 1 ; u <= N ; u++)
+            if(!used[u] && d[u] > d[v] + G[v][u]) d[u] = d[v] + G[v][u] ;//这里要检查一下
+            //d[u] = min(d[u] , d[v] + G[v][u]);
+    }
+}
+int main()
+{
+    scanf("%d%d",&T,&N);
+    for(int i = 1 ; i <= N ; i++)
+        for(int j = 1 ; j <= N ; j++)
+            G[i][j] = INF;
+
+    for(int i = 0 ; i < T ; i++){
+        int x , y , cost;
+        scanf("%d%d%d",&x,&y,&cost);
+        if(G[x][y] != INF) G[x][y] = min(G[x][y], cost);
+        else G[x][y] = cost;
+        G[y][x] = cost;
+    }
+    Dij();
+    printf("%d ",d[N]);
+}
