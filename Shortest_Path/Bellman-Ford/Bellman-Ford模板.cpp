b45370c6ee2afae15ac205536cc49619f7f37fb0
sync <zengzh1997@gmail.com>
2017-05-30 21:03:54 +0800

    Update Bellman-Ford模板.cpp

--- Shortest_Path/Bellman-Ford/Bellman-Ford模板.cpp
+++ Shortest_Path/Bellman-Ford/Bellman-Ford模板.cpp
@@ -1,4 +1,8 @@
- #include <bits/stdc++.h>
+ //#include <bits/stdc++.h>
+ #include <iostream>
+ #include <cstring>
+ //path 头文件
+ #include <stack>
 using namespace std;
 const int maxn = 1000;
 const int inf = 0x3f3f3f3f;
@@ -8,7 +12,7 @@ int path[maxn];
 int n , m;
 void Bellman(int v0){
     for(int i = 0 ;i < n ; i++){
-            d[i] = G[v0][i];
+            d[i] = G[v0][i];//这里应该有一步将d[v0] 设为 0
             if(i != v0 && d[i] < inf) path[i] = v0;//i != v0
             else path[i] = -1;
     }
@@ -30,6 +34,14 @@ int main()
 {
     memset(G , inf , sizeof(G));
     memset(d , inf , sizeof(d));
+
+
+//非必须步骤
+
+//    for(int i = 0 ; i < n ; i++)
+//        for(int j = 0 ; j < n ; j++)
+//            if(i == j) G[i][j] = 0;
+
     cin >> n ;
     int x , y ,dist;
     while(cin >> x >> y >> dist){
@@ -52,4 +64,6 @@ int main()
         }
         cout << s.top() << endl;
     }
+
+    //cout << d[0] <<endl;
 }
