d242faf22027676270d22547973b5fdeae4f3b6b
sync <zengzh1997@gmail.com>
2017-05-30 20:44:14 +0800

    Create Bellman-Ford模板.cpp

---
+++ Shortest_Path/Bellman-Ford/Bellman-Ford模板.cpp
@@ -0,0 +1,55 @@
+ #include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1000;
+const int inf = 0x3f3f3f3f;
+int d[maxn];
+int G[maxn][maxn];
+int path[maxn];
+int n , m;
+void Bellman(int v0){
+    for(int i = 0 ;i < n ; i++){
+            d[i] = G[v0][i];
+            if(i != v0 && d[i] < inf) path[i] = v0;//i != v0
+            else path[i] = -1;
+    }
+
+    for(int i = 2 ; i < n ;  i++){
+        for(int j = 0 ; j < n ; j++){
+            if(j == v0) continue;
+            for(int k = 0 ; k < n ; k++){
+                if(G[k][j] < inf && d[j] > d[k] + G[k][j]){
+                     d[j] = d[k] + G[k][j];
+                     path[j] = k;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    memset(G , inf , sizeof(G));
+    memset(d , inf , sizeof(d));
+    cin >> n ;
+    int x , y ,dist;
+    while(cin >> x >> y >> dist){
+        if(x == -1 && y == -1 && dist == -1) break;
+        G[x][y] = dist;
+    }
+    Bellman(0);
+
+    for(int i = 1 ; i < n ; i++){
+        cout << d[i] << "\t";
+        stack<int> s;
+        int t = i;
+        while(t != -1){
+            s.push(t);
+            t = path[t];
+        }
+        while(s.size() > 1){
+            cout << s.top() << " -> " ;
+            s.pop();
+        }
+        cout << s.top() << endl;
+    }
+}
